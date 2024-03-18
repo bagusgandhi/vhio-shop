d7a0b279b4f7fa7eb7bec38b12fdabe0ba1af258
bagusgandhi <bagusgandhi4@gmail.com>
2024-03-18 07:46:55 +0700

    feat: add cdn assets toasify-js

--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     @vite('resources/css/app.css')
+    <title>@yield('title')</title>
     @livewireStyles
 </head>
 
@@ -18,6 +19,7 @@
 
     @livewireScripts
     @vite('resources/js/app.js')
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 </body>
 
 </html>
